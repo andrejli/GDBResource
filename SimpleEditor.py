f3e3525c9bbcca1d118b5c2a97c0b611ac5c32d5
andrejli <sgroup@me.com>
2018-09-20 17:22:31 +0200

    Added Simple Container editor to database.

---
+++ SimpleEditor.py
@@ -0,0 +1,90 @@
+import os
+import hashlib
+
+
+class ContainerEditor(object):
+    """
+    Class opens small editor in console to enter document such as report or
+    more complex info to database GDPResource
+    """
+
+    def __init__(self):
+        self.plaintext = list()  # define text as empty variable (list of rows)
+        # M A I N   L O O P
+        while True:  # infinite loop
+            self.view_text()  # view given text in console
+            self.input_row()  # input new row
+
+    def input_row(self):
+        """
+        Method inputs new row to class variable self.plaintext
+        :return: row string (awaiting a command string)
+        """
+        row = input()  # built in function input string variable to console
+        self.check_command(row)  # check if row string is not command string
+        self.plaintext.append(row)  # append row string to list of rows stored in self.plaintext
+        return row  # returns row
+
+    def check_command(self, command):
+        """
+        Method awaits and serves as event handler for commands given from editor
+        :param command: command string (vim compatible)
+        :return: None
+        """
+        row = command  # defines row as a commnad given by input to console
+        if row == ':q':  # when command string equal this string DO:
+            quit()  # quit from editor
+        if row == ':wq':  # when command string equal this string DO:
+            self.save_all()  # saves all to file
+            quit()  # quit from editor
+
+    def view_text(self):
+        """
+        Method performs render of editor screen i terminal
+        :return:
+        """
+        os.system(command='clear')  # system command to clear screen in Linux
+        # TODO Make Win and Darwin compatible
+        [print(i) for i in self.plaintext]  # comprehension in Python to print all rows from self.plaintext
+        return  # return to main
+
+    def convert2bytes(self):
+        """
+        This method converts list of rows stored in self.plaintext to string.
+        Then converts string with linebreaks to bytes for further operations
+        :return: returns all text from self.plaintext as bytes
+        """
+        string = str()  # defines string as empty string variable
+        for i in self.plaintext:  # loops tru self.plaintext and DO:
+            string += i + '\n'  # add row plus linebreak symbol to string
+        # print(string)
+        result = bytes(string, encoding='utf8')  # encode string to final result bytes
+        return result  # return bytes with all text stored in variable self.plaintext
+
+    @staticmethod
+    def hash_it(text: bytes):
+        """
+        Method calculate md5 hash string from bytes
+        :param text: text in bytes
+        :return: string md5 hash algorithm
+        """
+        sha = hashlib.md5()  # Default algorithm
+        sha.update(text)  # add text and update claculation
+        result = sha.hexdigest()  # digest hexadecimal string
+        return result  # retuns strin to main
+
+    def save_all(self):
+        """
+        Method saves all data to file as binary
+        :return: if success returns True
+        """
+        text1nbytes = self.convert2bytes()  # convert text to bytes
+        filename = str(self.hash_it(text1nbytes)) + '.data'  # defines filename
+        with open(file=filename, mode='wb') as f:  # opens file in write binary mode
+            f.write(text1nbytes)  # write bytes to file
+            print('SAVED TO', filename)  #
+        return True
+
+
+if __name__ == '__main__':
+    obj = ContainerEditor()
